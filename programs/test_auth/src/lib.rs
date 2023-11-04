fcd658d28b1269ffb4fec01b342ae7845576ee21
Joe Ellul-Turner <joeetwork@gmail.com>
2023-11-04 10:40:41 +0000

    comments

--- programs/test_auth/src/lib.rs
+++ programs/test_auth/src/lib.rs
@@ -9,6 +9,7 @@ pub mod test_auth {
     pub fn initialize(ctx: Context<Initialize>, new_user: Pubkey) -> Result<()> {
         let user_profile = &mut ctx.accounts.user_profile;
 
+        //sets who can write to the user_profile account - in this case it will be new_user: pubkey.
         user_profile.authority = new_user;
 
         user_profile.data = 87;
@@ -44,8 +45,10 @@ pub struct Initialize<'info> {
 #[derive(Accounts)]
 pub struct ChangeUser<'info> {
     #[account(mut)]
+    //this authority verifies the authority is signed thus can only the owner of the public key can write to the user_profile.
     pub authority: Signer<'info>,
 
+    //has_one = authority checks the user_profile account variable authority is the same as the pubkey used to sign.
     #[account(
         mut,
         seeds = [b"USER", authority.key().as_ref()],
